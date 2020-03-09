b71341c6e6b1839c54d9d742d57167ea74337215
IgorOgievich <igorboog@mail.ru>
2020-03-09 21:25:46 +0300

    Add Props

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import TodoListTasks from "./TodoListTasks";
 import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
+    tasks = [
+        {title: "JS", isDone: true, priority:"medium"},
+        {title: "HTML", isDone: false, priority:"low"},
+        {title: "Css", isDone: true, priority:"low"},
+        {title: "React", isDone: false, priority:"low"}
+    ];
+    filterValue = "Completed";
+
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
                     <TodoListHeader/>
-                    <TodoListTasks/>
-                    <TodoListFooter/>
+                    <TodoListTasks tasks={this.tasks}/>
+                    <TodoListFooter filterValue ={this.filterValue}/>
                 </div>
             </div>
         );
--- src/TodoListTasks.js
+++ src/TodoListTasks.js
@@ -4,28 +4,19 @@ import TodoListTask from "./TodoListTask";
 
 class TodoListTasks extends React.Component {
     render = () => {
+        let taskElements = this.props.tasks.map(task => {
+                return (
+                    <TodoListTask title={task.title} isDone={task.isDone} priority={task.priority}/>
+                )
+            }
+        );
         return (
             <div className="todoList-tasks">
-                <TodoListTask title="JS" isDone={true} />
-                <TodoListTask title="JS" isDone={true}/>
-                <TodoListTask title="Css" isDone={true}/>
-                <TodoListTask title="React" isDone={false}/>
-                {/*<div className="todoList-task">*/}
-                {/*    <input type="checkbox" checked={true}/>*/}
-                {/*    <span>CSS</span>*/}
-                {/*</div>*/}
-                {/*<div className="todoList-task">*/}
-                {/*    <input type="checkbox" checked={false}/>*/}
-                {/*    <span>JS</span>*/}
-                {/*</div>*/}
-                {/*<div className="todoList-task">*/}
-                {/*    <input type="checkbox" checked={false}/>*/}
-                {/*    <span>ReactJS</span>*/}
-                {/*</div>*/}
-                {/*<div className="todoList-task">*/}
-                {/*    <input type="checkbox" checked={true}/>*/}
-                {/*    <span>Patterns</span>*/}
-                {/*</div>*/}
+                {taskElements}
+                {/*<TodoListTask title={this.props.tasks[0].title} isDone={this.props.tasks[0].isDone}/>*/}
+                {/*<TodoListTask title={this.props.tasks[1].title} isDone={this.props.tasks[1].isDone}/>*/}
+                {/*<TodoListTask title={this.props.tasks[2].title} isDone={this.props.tasks[2].isDone}/>*/}
+                {/*<TodoListTask title={this.props.tasks[3].title} isDone={this.props.tasks[3].isDone}/>*/}
             </div>)
     }
 }
